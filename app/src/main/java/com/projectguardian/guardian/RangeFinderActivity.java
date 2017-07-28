9423aaca7fa8fff8d2f03f0a5ea90d4d34b12c49
Philip Blumel <philipblumel@mail.weber.edu>
2017-07-27 21:13:03 -0600

    Request to enable Bluetooth on startup

--- app/src/main/java/com/projectguardian/guardian/RangeFinderActivity.java
+++ app/src/main/java/com/projectguardian/guardian/RangeFinderActivity.java
@@ -1,6 +1,9 @@
 package com.projectguardian.guardian;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
 import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -34,6 +37,9 @@ public class RangeFinderActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // If Bluetooth is disabled, prompt to enable.
+        enableBluetooth();
     }
 
     @Override
@@ -66,6 +72,21 @@ public class RangeFinderActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void enableBluetooth() {
+        // Initializes Bluetooth adapter.
+        BluetoothAdapter mBluetoothAdapter;
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);//REQUEST_ENABLE_BT);
+        }
     }
 
     /**
