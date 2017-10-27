06e822668fac2a1e40802f3d9c8353403576a0b8
Philip Blumel <philipblumel@mail.weber.edu>
2017-10-26 18:07:44 -0600

    Use IBEACON_FORMAT

--- app/src/main/java/com/projectguardian/guardian/BeaconInterface.java
+++ app/src/main/java/com/projectguardian/guardian/BeaconInterface.java
@@ -20,11 +20,11 @@ public class BeaconInterface extends Application implements BeaconConsumer {
     String TAG = "BeaconInterface";
     //Beacon Types
     // private static final String BEACON_FORMAT = "m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25";          // ALTBEACON
-    private static final String BEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25";             // ALTBEACON2
+    // private static final String BEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25";          // ALTBEACON2
     // private static final String BEACON_FORMAT =  "x,s:0-1=feaa,m:2-2=20,d:3-3,d:4-5,d:6-7,d:8-11,d:12-15";    // EDDYSTONE_TLM
     // private static final String BEACON_FORMAT = "s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19";               // EDDYSTONE_UID
     // private static final String BEACON_FORMAT = "s:0-1=feaa,m:2-2=10,p:3-3:-41,i:4-20v";                      // EDDYSTONE_URL
-    // private static final String BEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";                  // IBEACON
+    private static final String BEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";                  // IBEACON
 
     public String selectedBeacon = "0";
 
